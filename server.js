9431768c320ecb4abb074081feb03095b8d91c46
silan <siland.dope@yahoo.fr>
2020-01-14 13:43:13 -0800

    finish feature2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
-const express = require('espress');
+
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
 const cors = require('cors');
@@ -7,5 +9,30 @@ app.use(cors());
 
 
 const PORT = process.env.PORT || 3001;
-require('dotenv');
 
+app.get('/location', (request, response) => {
+  response.send('Happy!');
+});
+
+//   let location = {
+//     search_query: city,
+//     formatted_query:geoData[0].display_name.
+    
+//     longitude = geoData[0].lon, 
+//   }
+//   response.status (200).send (l)
+// });
+
+function location(city, locationData){
+  this.search_query = city;
+  this.latitude = locationData.lat;
+  this. longtitude = locationData.lon;
+}
+
+
+
+
+
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`Never Give up ${PORT}`));
