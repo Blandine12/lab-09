15dbd115ea488689416e6a7fc9ab96fd995a37e1
silan <siland.dope@yahoo.fr>
2020-01-15 13:39:10 -0800

    finished weather display

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 'use strict';
-
+const superagent = require('superagent');
 const express = require('express');
 const app = express();
 require('dotenv').config();
 
 const cors = require('cors');
+let locations ={};
 
 //  Declare variable
 app.use(cors());
@@ -17,43 +18,86 @@ app.use(cors());
 
 // define routes
 app.get('/location', (request, response) => {
-  response.send('Happy!');
-  try {
-    const dataArray = require('./data/geo.json');
-    const geoData = dataArray [0];
-    const city = request.query.city;
 
-    let location = new Location (city, geoData);
-    response.status(200).send(location);
+  try {
+    // const dataArray = require('./data/geo.json');
+    let city = request.query.city;
+    let key = process.env.GEOCODE_API_KEY;
+    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+
+    if (locations[url]) {
+      response.send(locations[url]);
+    }
+    else {
+      superagent.get(url)
+        .then(data => {
+          const geoData = data.body[0];
+          const location = new Location(city, geoData);
+          locations[url] = location;
+          response.send(location);
+
+        });
+    }
+    // const geoData = dataArray [0];
+    // const city = request.query.city;
+
+    // let location = new Location (city, geoData);
+    // response.status(200).send(location);
   }
   catch(error) {
     errorHandler('If you did not get results. Please try again.', response);
   }
 });
 
+
+
+
+
 app.get('/weather', (request, response) => {
 
-  try {
-    const weatherData = require('./data/darksky.json');
-    const dailyWeather = weatherData.daily.data.map(day => {
-      return new MapWeather(day);
-    });
-    // let dailyArray =[];
+  let latitude = request.query.latitude;
+  let longitude = request.query.longitude;
+  // let {latitude, longitude} = request.query;
 
-    // dailyWeather.forEach(day => {
-    //   dailyArray.push(new Weather(day));
-    // });
-    // response.status(200).send(dailyArray);
-    response.status(200).send(dailyWeather);
-  } catch(error) {
-    errorHandler('If you did not get result. Please try again.', response);
-  }
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${latitude},${longitude}`;
+
+  superagent.get(url)
+    .then(data => {
+      const dailyWeather = data.body.daily.data.map(day => {
+        return new MapWeather(day);
+      });
+      response.status(200).json(dailyWeather);
+    })
+    .catch(() => {
+      errorHandler('If you did not get result. Please, try again', request, response);
+    });
+
 });
 
+// app.get('/weather', (request, response) =>{
+
+//   try {
+//     const weatherData = require('./data/darksky.json');
+//     const dailyWeather = weatherData.daily.data.map(day => {
+//       return new MapWeather(day);
+//     });
+//     // let dailyArray =[];
+
+//     // dailyWeather.forEach(day => {
+//     //   dailyArray.push(new Weather(day));
+//     // });
+//     // response.status(200).send(dailyArray);
+//     response.status(200).send(dailyWeather);
+//   } catch(error) {
+//     errorHandler('If you did not get result. Please try again.', response);
+//   }
+// });
 
 
 
 // Define function
+
+
 
 function Location(city, localData) {
   this.search_query = city;
@@ -72,9 +116,6 @@ function MapWeather(dailyForecast) {
 function errorHandler(string, response) {
   response.status(500).send(string);
 }
-
-
-
 
 // Make sure the server is listening for requests
 app.listen(PORT, () => console.log(`Never Give up ${PORT}`));
