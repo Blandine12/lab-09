4790872b8e28a16be481204209c12f7b930908b7
silan <siland.dope@yahoo.fr>
2020-01-16 13:52:24 -0800

    try to resolve feature2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 require('dotenv').config();
 
 const cors = require('cors');
-let locations ={};
+let locations = {};
 
 // database connection set up
 const pg =require('pg');
@@ -19,41 +19,61 @@ const PORT = process.env.PORT || 3001;
 // use corss to allow to past data to front end
 
 app.use(cors());
-
+app.get('/location', locationHandler);
 
 
 // define routes
-app.get('/location', (request, response) => {
+function locationHandler(request, response) {
 
   try {
     // const dataArray = require('./data/geo.json');
     let city = request.query.city;
     let key = process.env.GEOCODE_API_KEY;
     const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+    let SQL = 'select * from locations where search_query = $1;';
+    let values = [city];
 
-    if (locations[url]) {
-      response.send(locations[url]);
-    }
-    else {
-      superagent.get(url)
-        .then(data => {
-          const geoData = data.body[0];
-          const location = new Location(city, geoData);
-          locations[url] = location;
-          response.send(location);
+  
 
-        });
-    }
-    // const geoData = dataArray [0];
-    // const city = request.query.city;
+    return client.query(SQL, values)
+      .then(result => {
+        // console.log(result);
 
-    // let location = new Location (city, geoData);
-    // response.status(200).send(location);
+        // if there is a city in DB
+        // results.rows = ['seattle', '55585']
+
+        // if theres no city in the DB
+        // results.rowss = [];
+        if (result.rowCount) {
+          console.log(result.rows);
+          response.send(result.rows[0]);
+        }
+        // hit this condition when there isn't city searched in database
+        else {
+          superagent.get(url)
+            .then(data => {
+              // add to DB
+              const geoData = data.body[0];
+              const location = new Location(city, geoData);
+              locations[url] = location;
+              response.send(location);
+            })
+            .catch(() => {
+              errorHandler('If you did not get result. Please, try again', request, response);
+            });
+
+
+        }
+
+      });
+
+
   }
   catch(error) {
     errorHandler('If you did not get results. Please try again.', response);
   }
-});
+}
+
 
 
 
@@ -72,33 +92,13 @@ app.get('/weather', (request, response) => {
       const dailyWeather = data.body.daily.data.map(day => {
         return new MapWeather(day);
       });
-      response.status(200).json(dailyWeather);
+      response.status(200).send(dailyWeather);
     })
     .catch(() => {
       errorHandler('If you did not get result. Please, try again', request, response);
     });
 
 });
-
-// app.get('/weather', (request, response) =>{
-
-//   try {
-//     const weatherData = require('./data/darksky.json');
-//     const dailyWeather = weatherData.daily.data.map(day => {
-//       return new MapWeather(day);
-//     });
-//     // let dailyArray =[];
-
-//     // dailyWeather.forEach(day => {
-//     //   dailyArray.push(new Weather(day));
-//     // });
-//     // response.status(200).send(dailyArray);
-//     response.status(200).send(dailyWeather);
-//   } catch(error) {
-//     errorHandler('If you did not get result. Please try again.', response);
-//   }
-// });
-
 
 
 // Define function
@@ -126,6 +126,7 @@ function errorHandler(string, response) {
 // Make sure the server is listening for requests
 // app.listen(PORT, () => console.log(`Never Give up ${PORT}`));
 
+// connect to BD and Start the web server
 client.connect()
   .then(() => {
     app.listen(PORT, () => {
